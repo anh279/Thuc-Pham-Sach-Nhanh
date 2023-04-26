59fb6695909f945f1211e44a530a77b1936eda3c
anh279 <nguoikhac45@gmail.com>
2023-04-26 08:53:40 +0700

    Xong phần đăng ký đăng nhập

--- main.js
+++ main.js
@@ -14,14 +14,35 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-var index = 0;
-var changeImage = function () {
-  var imgs = ["./assets/img/Banner1.png", "./assets/img/Banner2.png"];
-  document.getElementById("image").src = imgs[index];
-  index++;
-  if (index == 2) {
-    index = 0;
-  }
-};
+function signup(e) {
+  event.preventDefault();
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var tel = document.getElementById("tel").value;
+  var user = {
+    username: username,
+    email: email,
+    password: password,
+    tel: tel,
+  };
+  var json = JSON.stringify(user);
+  localStorage.setItem(username, json);
+  alert("Đăng ký thành công");
+}
 
-setInterval(changeImage, 3000);
+function login(e) {
+  event.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var user = localStorage.getItem(username);
+  var data = JSON.parse(user);
+  if (user == null) {
+    alert("Vui lòng nhập đầy đủ thông tin");
+  } else if (username == data.username && password == data.password) {
+    alert("Đăng nhập thành công");
+    window.location.href = "index.html";
+  } else {
+    alert("Thông tin không chính xác");
+  }
+}
